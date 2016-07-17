bc54b1d127bf99af3b18b11ffdc7c9de10e1df03
Jacob Meixner <jjm3333@gmail.com>
2016-07-17 16:15:03 -0500

    Add seciton to the home page for work and resume

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # mySite
 This is a repo for my psersonal website
+
+During setup add /public/files/jacobjmeixner2016resume.doc
--- views/index.jsx
+++ views/index.jsx
@@ -6,9 +6,19 @@ var HelloMessage = React.createClass({
         return (
             <DefaultLayout title={this.props.title}>
                 <div>
-                    Hello {this.props.title},
+                    <h1>Hello {this.props.title},</h1>
                     <br/>
-                    Welcome to the web site for Jacob J Meixner a Software engineer and computer enthusiast 
+                    <p>Welcome to the web site for Jacob J Meixner, a Software engineer and computer enthusiast.</p>
+
+                    <p>For my recent work take a loog at my <a href="https://github.com/jjm3x3/">GitHub</a>
+                        <p>
+                            <h2>Intereted in hiring me?</h2>
+                                <ul>
+                                    <li>View my <a href="https://www.linkedin.com/in/jacob-meixner-10359b82">LinkedIn</a></li>
+                                    <li>Download my latest <a href="/files/jacobjmeixner2016resume.doc">Resume</a></li>
+                                </ul>
+                        </p>
+                    </p>
 
 
                 </div>
