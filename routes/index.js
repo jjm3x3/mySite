58c2b2eb333fc16a96bbf28d03507b89b67ba7dd
Jacob Meixner <jjm3333@gmail.com>
2016-09-17 13:06:00 -0700

    Add abstraction to blog posts.
    they will now be stored in a mongo db. This is my first use of mongodb
    be gental :). but really this should make it easier to render more and
    more blog posts as they get written. and this is just the first step to
    having this mechanism work fully

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
+var mongoClient = require('mongodb');
+
+var URL = 'mongodb://localhost:27017/mySite'
+var POSTDOC = 'post'
+// OH so this is what happens after school
+   
+           Array.prototype.flatMap = function(lambda) {
+               return Array.prototype.concat.apply([], this.map(lambda));
+           };
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Fellow User', downloadUrl: 'http://' + appDomain + '/downloads'});
+    mongoClient.connect(URL, function(err, db) {
+        var collection = db.collection('post');
+        collection.find({}).toArray(function(err,docs) {
+            postList = docs.map(function(blogPost){
+                blogParas = blogPost.post.split("\n")
+                blogParas.push(blogPost.signature)
+                return blogParas
+            });
+            res.render('index', { title: 'Fellow Traveler', downloadURL: 'http://' + appDomain + '/downloads',
+                                  postList: postList});
+        });
+    })
+
+});
+
+router.get('/dothing', function(req,res,next) {
+    res.status(404)
+    res.render("");
 });
 
 router.get('/downloads', function(req, res, next) {
--- views/index.jsx
+++ views/index.jsx
@@ -16,25 +16,24 @@ var HelloMessage = React.createClass({
                     </ul>
 
                     <h2>Blog</h2>
-		    <p>This first post is really just an introduction to me. I am a programmer by trade and the main purpose of this website is to give myself an online presence. As a young professional in the technology industry, it seems that the best way to make a name for yourself outside of doing good work and contributing to open source is to host a personal website. Another reason I am writing this website is because I hope to improve my skills on the web platform. This is a goal of mine because there seems to be quite a move to make everything from your thermostat to your car available over the web, making it a hugely relevant platform for programmers like myself.</p>
-		    <p>This page is written in express and served using Nginx. Using express was to satisfy a curiosity I have about writing websites in JavaScript. I have written a number of REST APIs in Ruby on Rails so I am quite familiar with how these web frameworks work at a high level. However, I have always been kind of interested in writing more JavaScript because as much as I can appreciate ruby and its flexibility, I am still somewhat partial to the java-like syntax that JavaScript brings to the table.</p>
-<p>Another new-ish technology that I am writing this site with is React. I understand that most people just go with the MEAN stack for things like this but I am not a very big fan of angular and I have only minimally used react. So, I wanted to take this opportunity to improve those skills and improve my knowledge of the framework.</p>
-<p>I will try to make a post to this blog at least once per week just to share some cool things I am looking at and getting into but I am going to sign off for right now. As stated above, I hope to make this site multifaceted but some of theses features will come in due time. That's all for now. Thanks for reading and stay tuned.</p>
-
-                    <p> Jacob Meixner 09-03-2016</p>
+		    {this.props.postList.map(function(post){
+                         return (<div>{post.map(function(para){
+                                 return <p>{para}</p>
+                             })}
+                             <hr></hr>
+                         </div>)
+                     })}
                     
 
                     <p>For my recent work take a look at my <a href="https://github.com/jjm3x3/">GitHub</a></p>
-                    <p>If you are here looking for the latest release of SimpleAccountant go  <a href={this.props.downloadUrl }>here</a>
+                    <p>If you are here looking for the latest release of SimpleAccountant go  <a href={this.props.downloadUrl }>here</a></p>
 
-                        <p>
-                            <h2>Interested in hiring me?</h2>
-                                <ul>
-                                    <li>View my <a href="https://www.linkedin.com/in/jacob-meixner-10359b82">LinkedIn</a></li>
-                                    <li>Download my latest <a href="/files/jacobjmeixner2016resume.doc">Resume</a></li>
-                                </ul>
-                        </p>
-                    </p>
+                        <h2>Interested in hiring me?</h2>
+                        <ul>
+                            <li>View my <a href="https://www.linkedin.com/in/jacob-meixner-10359b82">LinkedIn</a></li>
+                            <li>Download my latest <a href="/files/jacobjmeixner2016resume.doc">Resume</a></li>
+                        </ul>
+                        {/* </p> */}
 
 
                 </div>
